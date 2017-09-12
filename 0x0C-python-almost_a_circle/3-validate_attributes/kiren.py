88bd51c7882b2313446473ea431eabf4ce2290c3
Kiren Srinivasan <srinitude@gmail.com>
2017-09-12 17:08:45 +0000

    Adds invalid Rectangle init args

--- 0x0C-python-almost_a_circle/3-validate_attributes/kiren.py
+++ 0x0C-python-almost_a_circle/3-validate_attributes/kiren.py
@@ -9,23 +9,6 @@ from models.rectangle import Rectangle
 
 
 class TestRectangle(unittest.TestCase):
-    """Initialization tests with valid arguments"""
-    def test_valid_height_width(self):
-        rect = Rectangle(10, 2)
-        self.assertEqual(rect.id, 2)
-        self.assertEqual(rect.x, 0)
-        self.assertEqual(rect.y, 0)
-        self.assertEqual(rect.width, 10)
-        self.assertEqual(rect.height, 2)
-
-    def test_all_valid_params(self):
-        rect = Rectangle(10, 2, 3, 4, 12)
-        self.assertEqual(rect.id, 12)
-        self.assertEqual(rect.x, 3)
-        self.assertEqual(rect.y, 4)
-        self.assertEqual(rect.width, 10)
-        self.assertEqual(rect.height, 2)
-
     """Initialization tests with invalid arguments"""
     with self.assertRaises(TypeError):
         rect = Rectangle(10, "2")
