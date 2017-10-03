9eff5400dccc5e910adb39e69aed2838d8509323
alexaorrico <alexaorrico@gmail.com>
2017-10-03 20:33:51 +0000

    test is subclass of BaseModel, test for all attrs

--- AirBnB_clone/tests/test_models/test_place.py
+++ AirBnB_clone/tests/test_models/test_place.py
@@ -6,6 +6,7 @@ Contains the TestPlaceDocs classes
 from datetime import datetime
 import inspect
 from models import place
+from models.base_model import BaseModel
 import pep8
 import unittest
 Place = place.Place
@@ -53,3 +54,87 @@ class TestPlaceDocs(unittest.TestCase):
                              "{:s} method needs a docstring".format(func[0]))
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method needs a docstring".format(func[0]))
+
+
+class TestPlace(unittest.TestCase):
+    """Test the Place class"""
+    def test_is_subclass(self):
+        """Test that Place is a subclass of BaseModel"""
+        place = Place()
+        self.assertIsInstance(place, BaseModel)
+        self.assertTrue(hasattr(place, "id"))
+        self.assertTrue(hasattr(place, "created_at"))
+        self.assertTrue(hasattr(place, "updated_at"))
+
+    def test_city_id_attr(self):
+        """Test Place has attr city_id, and it's an empty string"""
+        place = Place()
+        self.assertTrue(hasattr(place, "city_id"))
+        self.assertEqual(place.city_id, "")
+
+    def test_user_id_attr(self):
+        """Test Place has attr user_id, and it's an empty string"""
+        place = Place()
+        self.assertTrue(hasattr(place, "user_id"))
+        self.assertEqual(place.user_id, "")
+
+    def test_name_attr(self):
+        """Test Place has attr name, and it's an empty string"""
+        place = Place()
+        self.assertTrue(hasattr(place, "name"))
+        self.assertEqual(place.name, "")
+
+    def test_description_attr(self):
+        """Test Place has attr description, and it's an empty string"""
+        place = Place()
+        self.assertTrue(hasattr(place, "description"))
+        self.assertEqual(place.description, "")
+
+    def test_number_rooms_attr(self):
+        """Test Place has attr number_rooms, and it's an int == 0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "number_rooms"))
+        self.assertEqual(type(place.number_rooms), int)
+        self.assertEqual(place.number_rooms, 0)
+
+    def test_number_bathrooms_attr(self):
+        """Test Place has attr number_bathrooms, and it's an int == 0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "number_bathrooms"))
+        self.assertEqual(type(place.number_bathrooms), int)
+        self.assertEqual(place.number_bathrooms, 0)
+
+    def test_max_guest_attr(self):
+        """Test Place has attr max_guest, and it's an int == 0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "max_guest"))
+        self.assertEqual(type(place.max_guest), int)
+        self.assertEqual(place.max_guest, 0)
+
+    def test_price_by_night_attr(self):
+        """Test Place has attr price_by_night, and it's an int == 0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "price_by_night"))
+        self.assertEqual(type(place.price_by_night), int)
+        self.assertEqual(place.price_by_night, 0)
+
+    def test_latitude_attr(self):
+        """Test Place has attr latitude, and it's a float == 0.0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "latitude"))
+        self.assertEqual(type(place.latitude), float)
+        self.assertEqual(place.latitude, 0.0)
+
+    def test_latitude_attr(self):
+        """Test Place has attr longitude, and it's a float == 0.0"""
+        place = Place()
+        self.assertTrue(hasattr(place, "longitude"))
+        self.assertEqual(type(place.longitude), float)
+        self.assertEqual(place.longitude, 0.0)
+
+    def test_amenity_ids_attr(self):
+        """Test Place has attr amenity_ids, and it's an empty list"""
+        place = Place()
+        self.assertTrue(hasattr(place, "amenity_ids"))
+        self.assertEqual(type(place.amenity_ids), list)
+        self.assertEqual(len(place.amenity_ids), 0)
